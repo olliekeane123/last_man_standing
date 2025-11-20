baa2cd530505d5c2c553a9d3ea7ca264cc6d9348
olliekeane123 <ollie.keane1@gmail.com>
2025-11-20 23:42:56 +0000

    add upsert gameweek to syncFixtures logic

--- src/lib/services/fixtureService.ts
+++ src/lib/services/fixtureService.ts
@@ -1,6 +1,7 @@
 import prisma from "@/lib/prisma"
 import { fetchFixturesForCompetition } from "@/lib/external-api/footballData/fixtures"
 import { FootballFixture } from "@/lib/types/schemas/footballApiSchemas"
+import { Prisma } from "@prisma/client"
 
 export async function syncAllFixtures() {
     const competitions = await prisma.competition.findMany()
@@ -29,7 +30,7 @@ export async function syncAllFixturesForCompetition(
             competitionCode
         )
 
-        const batchSize = 50
+        const batchSize = 10
         for (let i = 0; i < externalFixtures.length; i += batchSize) {
             const batch = externalFixtures.slice(i, i + batchSize)
             console.log(
@@ -38,41 +39,68 @@ export async function syncAllFixturesForCompetition(
                 )}`
             )
             await prisma.$transaction(
-                batch.map((fixture) =>
-                    prisma.fixture.upsert({
-                        where: { apiId: fixture.id },
-                        update: {
-                            status: fixture.status,
-                            matchday: fixture.matchday,
-                            utcDate: new Date(fixture.utcDate),
-                            score: fixture.score,
-                            apiLastUpdated: new Date(fixture.lastUpdated),
-                        },
-                        create: {
-                            apiId: fixture.id,
-                            Competition: {
-                                connect: { apiId: fixture.competition.id },
-                            },
-                            homeTeam: {
-                                connect: { apiId: fixture.homeTeam.id },
-                            },
-                            awayTeam: {
-                                connect: { apiId: fixture.awayTeam.id },
-                            },
-                            utcDate: new Date(fixture.utcDate),
-                            status: fixture.status,
-                            matchday: fixture.matchday,
-                            score: fixture.score,
-                            apiLastUpdated: new Date(fixture.lastUpdated),
-                        },
-                    })
-                )
+                async (tx) => {
+                    await Promise.all(
+                        batch.map((fixture) => upsertFixture(fixture, tx))
+                    )
+                },
+                {
+                    timeout: 20000,
+                }
             )
         }
 
         return externalFixtures
     } catch (error) {
         console.error("Failed to store fixtures:", error)
+        throw error
+    }
+}
+
+async function upsertFixture(
+    rawFixtureData: FootballFixture,
+    tx: Prisma.TransactionClient
+): Promise<void> {
+    try {
+        const gameweek = await tx.gameweek.upsert({
+            where: { gameweekNumber: rawFixtureData.matchday },
+            update: {},
+            create: {
+                gameweekNumber: rawFixtureData.matchday,
+                deadline: new Date().toISOString(),
+            },
+        })
+
+        await tx.fixture.upsert({
+            where: { apiId: rawFixtureData.id },
+            update: {
+                status: rawFixtureData.status,
+                matchday: rawFixtureData.matchday,
+                utcDate: new Date(rawFixtureData.utcDate),
+                score: rawFixtureData.score,
+                gameweek: { connect: { id: gameweek.id } },
+                apiLastUpdated: new Date(rawFixtureData.lastUpdated),
+            },
+            create: {
+                apiId: rawFixtureData.id,
+                Competition: {
+                    connect: { apiId: rawFixtureData.competition.id },
+                },
+                homeTeam: {
+                    connect: { apiId: rawFixtureData.homeTeam.id },
+                },
+                awayTeam: {
+                    connect: { apiId: rawFixtureData.awayTeam.id },
+                },
+                utcDate: new Date(rawFixtureData.utcDate),
+                status: rawFixtureData.status,
+                matchday: rawFixtureData.matchday,
+                score: rawFixtureData.score,
+                gameweek: { connect: { id: gameweek.id } },
+                apiLastUpdated: new Date(rawFixtureData.lastUpdated),
+            },
+        })
+    } catch (error) {
         throw error
     }
 }
