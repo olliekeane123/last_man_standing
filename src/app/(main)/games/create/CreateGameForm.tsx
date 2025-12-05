8208c0d2a79ffb7256c850aa48dd79869b72623c
olliekeane123 <ollie.keane1@gmail.com>
2025-12-05 16:40:01 +0000

    remove unused label import

--- src/app/(main)/games/create/CreateGameForm.tsx
+++ src/app/(main)/games/create/CreateGameForm.tsx
@@ -7,7 +7,7 @@ import { CreateGameFormData } from "@/lib/types/game"
 import { createGameAction } from "@/lib/actions/game.actions"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+
 import {
     Form,
     FormField,
